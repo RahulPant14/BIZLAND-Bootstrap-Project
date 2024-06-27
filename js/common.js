40e79a7a96f2ba3479d9cfdacb0f1b6ca78497e0
rahulpant <rohanpant80@gmail.com>
2024-06-27 13:30:57 +0530

    CSS commited

---
+++ js/common.js
@@ -0,0 +1,31 @@
+$(function(){
+
+    $(".wrap-10 .tabs-imp a").click(function(e){
+        console.log(2)
+        e.preventDefault();
+        $(".wrap-10 .tabs-imp a").removeClass("active");
+        $(this).addClass("active");
+
+        if($(this).text()=="All"){
+            $(".wrap-10 .all").fadeIn();
+        }
+        else if($(this).text()=="APP"){
+            $(".wrap-10 .cards").fadeOut(0);
+            $(".wrap-10 .web").fadeOut(0);
+            $(".wrap-10 .app").fadeIn();
+         }
+        else if($(this).text()=="CARD"){ 
+            $(".wrap-10 .app").fadeOut(0);
+            $(".wrap-10 .web").fadeOut(0);
+            $(".wrap-10 .cards").fadeIn();
+        }
+        else if($(this).text()=="WEB"){
+            $(".wrap-10 .app").fadeOut(0);
+            $(".wrap-10 .cards").fadeOut(0);
+            $(".wrap-10 .web").fadeIn();
+         }
+    });
+
+    AOS.init();
+
+});
