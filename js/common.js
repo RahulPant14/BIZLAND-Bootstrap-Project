f71540b2d6683dc7ef5b43723111fb5684cb0bb1
rahulpant <rohanpant80@gmail.com>
2024-06-28 14:34:36 +0530

    images correction done

--- js/common.js
+++ js/common.js
@@ -1,12 +1,13 @@
 $(function(){
 
+
+
     $(".wrap-10 .tabs-imp a").click(function(e){
-        console.log(2)
         e.preventDefault();
         $(".wrap-10 .tabs-imp a").removeClass("active");
         $(this).addClass("active");
 
-        if($(this).text()=="All"){
+        if($(this).text()=="ALL"){
             $(".wrap-10 .all").fadeIn();
         }
         else if($(this).text()=="APP"){
